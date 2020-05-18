830b5f267c77ca01af4996a254a61a34fd7f79e5
student <student@learncodinganywhere.com>
2020-05-18 09:19:47 -0700

    Assignment 2 work

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            
+            Console.WriteLine("Student Daily Report");
+            
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+
+            Console.WriteLine("What page Number?");
+            string pageNumber = Console.ReadLine();
+
+            Console.WriteLine("Do you need help with anything? Answer 'True' or 'False'.");
+            string helpWithAnything = Console.ReadLine();
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string positiveExperiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
