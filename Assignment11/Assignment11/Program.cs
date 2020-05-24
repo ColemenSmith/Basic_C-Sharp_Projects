2b534446c4f16bf6c72fca1d6a3f90d93a03496f
student <student@learncodinganywhere.com>
2020-05-24 09:02:57 -0700

    Try/Catch

---
+++ Assignment11/Assignment11/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] intArray = { 4, 7, 10, 15, 20 };
+
+            Console.WriteLine("Entering try/catch.");
+            Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Enter a number to divide into the list.");
+                int dividend = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int num in intArray)
+                {
+                    Console.WriteLine(num / dividend);
+                }
+                Console.ReadLine();
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You cannot divide by zero");
+                return;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter your choice as a whole number.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.WriteLine("Exiting try/catch");
+            Console.ReadLine();
+        }
+    }
+}
