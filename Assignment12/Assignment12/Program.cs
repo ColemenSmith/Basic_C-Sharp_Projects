8052a1e223822946ea204474c99a0a90140fcd9d
student <student@learncodinganywhere.com>
2020-05-25 16:07:58 -0700

    Classes and objects work

---
+++ Assignment12/Assignment12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to do operations on?");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int timesThree = RandomNumber.timesBy3(num);
+            Console.WriteLine(num + " multiplied by 3 = " + timesThree);
+            Console.ReadLine();
+
+            int bunchaStuff = RandomNumber.bunchaStuff(num);
+            Console.WriteLine(num + " multiplied by 6 then 3 subtracted from it and then added to 10 = " + bunchaStuff);
+            Console.ReadLine();
+
+            int evenOrOdd = RandomNumber.evenOrOdd(num);
+            
+            Console.WriteLine("Now we will figure out if your number is even or odd. You entered " + num + " which after i put it into my magical equation returns " + evenOrOdd + " so it is: ");
+            Console.ReadLine();
+            if (evenOrOdd == 1)
+            {
+                Console.WriteLine("odd");
+            }
+            else
+            {
+                Console.WriteLine("even");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment12/Assignment12/RandomNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment12
+{
+    class RandomNumber
+    {
+        public static int timesBy3(int num)
+        {
+            return num * 3;
+        }
+
+        public static int bunchaStuff(int num)
+        {
+            return num * 6 - 3 + 10;
+        }
+
+        public static int evenOrOdd(int num)
+        {
+            return num % 2;
+        }
+    }
+}
