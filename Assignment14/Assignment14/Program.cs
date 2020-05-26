a6d55d1140b1e6866033afff3e4b8655f06ea2af
ColemenSmith <smithcolemen@gmail.com>
2020-05-25 21:10:40 -0700

    Assignment 14

---
+++ Assignment14/Assignment14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Please enter one or two numbers, the second is optional.");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Would you like to enter a second number?");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "yes")
+            {
+                Console.WriteLine("Enter your second number");
+                int second = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(randomMethod.randomOperation(first, second));
+            }
+            else if (answer == "no")
+            {
+                Console.WriteLine(randomMethod.randomOperation(first));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Assignment14/Assignment14/randomMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment14
+{
+    class randomMethod
+    {   
+        public static int randomOperation(int first, int second = 7)
+        {
+            return first * second + 2;
+        }
+    }
+}
