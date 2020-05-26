c3a35a0620ecc1b297a9c3624fe27ca899d9500c
ColemenSmith <smithcolemen@gmail.com>
2020-05-25 20:49:09 -0700

    Assignment 13

---
+++ Assignment13/Assignment13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to be put through a test.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Now please enter a decimal number to go through a test.");
+            decimal dec = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("And a final number to go through a test.");
+            string written = Console.ReadLine();
+
+            Console.WriteLine(num + " goes into the method and out comes: " + RandomOperation.multiplier(num));
+            Console.ReadLine();
+            Console.WriteLine(dec + " goes into the second method and out comes: " + RandomOperation.multiplier(dec));
+            Console.ReadLine();
+            Console.WriteLine(written + " goes into the third method and out comes: " + RandomOperation.multiplier(written));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment13/Assignment13/RandomOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13
+{
+    class RandomOperation
+    {
+        public static int multiplier(int num)
+        {
+            return num * 8;
+        }
+
+        public static decimal multiplier(decimal dec)
+        {
+            return dec * 8.32M;
+        }
+
+        public static int multiplier(string written)
+        {
+            int temp = Int32.Parse(written);
+            return temp * 12;
+        }
+    }
+}
