c1b8adb863f38fd1b4e846780ee3573e54a9f238
ColemenSmith <smithcolemen@gmail.com>
2020-05-26 19:51:37 -0700

    Assignment 15
    more method work

---
+++ Assignment15/Assignment15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            assignment.testMethod(3, 7);
+            Console.ReadLine();
+
+            assignment.testMethod(num: 4, num2: 9);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment15/Assignment15/assignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment15
+{
+    class assignment
+    {
+        public static void testMethod(int num, int num2)
+        {
+            int operation = 9 * num;
+            Console.WriteLine(num2);
+        }
+    }
+}
