9fb45882914ea6e542093fdb2294402b712826e1
ColemenSmith <smithcolemen@gmail.com>
2020-06-01 12:45:36 -0700

    Created a struct`
    Created a struct and then created an object type of the property of the struct

---
+++ Assignment19/Assignment19/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment19
+{
+    struct Number
+    {
+        public decimal Amount;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number value = new Number();
+            value.Amount = 2.22m;
+            Console.WriteLine(value.Amount);
+            Console.ReadLine();
+        }
+    }
+}
