998ff3398a7c957ccc761700a31800ec1e1cefff
ColemenSmith <smithcolemen@gmail.com>
2020-06-01 14:00:30 -0700

    Integrating File I/O
    Integrated file io into game so it keeps track of cards dealt

--- ClassesAndObjects/ClassesAndObjects/Dealer.cs
+++ ClassesAndObjects/ClassesAndObjects/Dealer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ClassesAndObjects
 {
@@ -15,7 +16,12 @@ namespace ClassesAndObjects
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\smith\Desktop\Basic_C-Sharp_Projects\ClassesAndObjects\Logs\Log.txt", true))
+            {
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
         }
     }
