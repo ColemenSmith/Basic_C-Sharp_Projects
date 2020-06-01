4e1ea5e74f7340d738f05e3fe123fa239bb407b3
ColemenSmith <smithcolemen@gmail.com>
2020-06-01 13:59:55 -0700

    File I/O
    Using file IO to add text to a document and then print that text to the console.

---
+++ Assignment21/Assignment21/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string number = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\smith\Desktop\Basic_C-Sharp_Projects\Assignment21\Log.txt", number);
+
+            Console.WriteLine(File.ReadAllText(@"C:\Users\smith\Desktop\Basic_C-Sharp_Projects\Assignment21\Log.txt"));
+
+            Console.ReadLine();
+        }
+    }
+}
