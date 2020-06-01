1b84d089b1306633eeebe86ec3e2589cea2502cc
ColemenSmith <smithcolemen@gmail.com>
2020-06-01 14:14:47 -0700

    DateAndTime practice

---
+++ Assignment22/Assignment22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("It will be " + DateTime.Now.AddHours(number) + " in " + number + " hours.");
+
+            Console.ReadLine();
+        }
+    }
+}
